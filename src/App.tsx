bf5641743c062d17723a907dd93c1f6278c5ad25
Paulo Guerra <paulo.guerra@verndale.com>
2022-09-13 21:15:46 +0200

    :sparkles: -- add simple netlify form

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { FormEvent, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({ name: "" });
+  const submitForm = async (form: HTMLFormElement) => {
+    console.log("sending", form, formData);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
+
+  const handleFormSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    submitForm(e.target as HTMLFormElement);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        onSubmit={handleFormSubmit}
+      >
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <label>
+            Your Name:{" "}
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={(e) => {
+                const { value } = e.currentTarget;
+                setFormData({ name: value });
+              }}
+            />
+          </label>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
